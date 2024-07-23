dc089c7645e1ba224fea3737b96772b1d10406fa
Maxhelbes <Maxhelbes@yandex.ru>
2024-07-23 13:59:18 +0300

    add grid_screen, add photo_screen, add ModalBottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:surf_flutter_summer_school_24/grid_screen.dart';
+import 'package:surf_flutter_summer_school_24/photo_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,78 +11,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: PhotoPage(),
-    );
-  }
-}
-
-const urlList = [
-  'https://wallpapers.com/images/hd/best-ipad-detailed-forest-whrhg2prdhu1224u.jpg',
-  'https://wallpapers.com/images/hd/pine-tree-forest-minimalist-android-r8hj5r0xfc5osrpo.jpg',
-  'https://wallpapers.com/images/hd/watch-tower-in-forest-indie-phone-qc8emhngrxxiligv.jpg',
-  'https://i1.wp.com/wallpapercave.com/wp/wp5527045.jpg',
-  'https://wallpapers.com/images/hd/iphone-7-plus-h05npj7q7ryyp27a.jpg',
-];
-class PhotoPage extends StatefulWidget {
-  const PhotoPage({super.key});
-
-  @override
-  State<PhotoPage> createState() => _PhotoPageState();
-}
-
-class _PhotoPageState extends State<PhotoPage> {
-  int _numberCurrentImage = 1;
-  final _countImages = urlList.length;
-
-  void _changeCurrentNumber(int number) {
-    setState(() {
-      _numberCurrentImage = number + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('22.07.2023'),
-        leading: const Icon(Icons.arrow_back),
-        actions: [Text('$_numberCurrentImage/$_countImages')],
-      ),
-      body: Center(
-        child: PageView.builder(
-          itemCount: urlList.length,
-          controller: PageController(viewportFraction: 0.9),
-          onPageChanged: (value) => _changeCurrentNumber(value),
-          itemBuilder: (BuildContext context, int itemIndex) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: ImageTile(url: urlList[itemIndex]),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-
-
-class ImageTile extends StatelessWidget {
-  final String url;
-
-  const ImageTile({
-    super.key,
-    required this.url,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ClipRRect(
-          borderRadius: BorderRadius.circular(30.0),
-          child: Image.network(url, fit: BoxFit.cover)),
+    return  MaterialApp(
+      home: GridScreen(),
     );
   }
 }
